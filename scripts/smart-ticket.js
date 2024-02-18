6d5ec518014e72285be363d2c35c4a8c4134807d
rahidhaque <ontirahid@yahoo.com>
2024-02-18 17:32:39 +0600

    Max 4 seats can be selected and same seat can't be selected twice functionality created

--- scripts/smart-ticket.js
+++ scripts/smart-ticket.js
@@ -1,8 +1,28 @@
+let selectedSeats = [];
+let selectedSeatsCount= 0;
 function handleGetTicketId(event){
+    event.stopPropagation();
     if (event.target.tagName === 'BUTTON') {
         const buttonId = event.target.id;
         setBackgroundColor(buttonId);
+
+        
+        if (selectedSeats.includes(buttonId)) {
+            alert(`Seat of id ${buttonId} is already selected`);
+            return;
+        }
+
+
+        if(selectedSeatsCount<3){
+            selectedSeatsCount++;
+            selectedSeats.push(buttonId);
+        }
+
+        else{
+            alert("Maximum Sit Limit Reached");
+            return;
+        }
     }
 }
 
-getElement('sit-info').addEventListener('click', handleGetTicketId);
+document.addEventListener('click', handleGetTicketId);
